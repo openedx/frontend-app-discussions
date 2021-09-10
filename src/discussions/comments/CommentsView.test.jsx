457be88f3badfe0548b64ae30925b3f9b9adecc2
Maxim Beder <maxim@opencraft.com>
2021-09-11 00:22:13 +0200

    test: mock paged comments
    Mocked axios to return paged comments.

--- src/discussions/comments/CommentsView.test.jsx
+++ src/discussions/comments/CommentsView.test.jsx
@@ -3,20 +3,86 @@
  */
 import React from 'react';
 import { MemoryRouter, Route } from 'react-router';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import MockAdapter from 'axios-mock-adapter';
 import { initializeMockApp } from '@edx/frontend-platform';
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 import { AppProvider } from '@edx/frontend-platform/react';
-import { initializeStore } from '../../store';
 import { IntlProvider } from 'react-intl';
+
+import { initializeStore } from '../../store';
+import { commentsApiUrl } from './data/api';
+import { selectThreadComments } from './data/selectors';
 
 import CommentsView from './CommentsView';
 
-const postId = 1;
+const postId = '1';
 let container;
 let store;
 let axiosMock;
+
+const mockCommentsPaged = [
+  [
+    {
+      threadId: postId,
+      id: '1',
+      renderedBody: 'test comment 1',
+      voteCount: 0,
+      author: 'testauthor',
+      users: {
+        'testauthor': {
+          profile: {
+            image: {
+              image_url_small: '',
+            },
+          },
+        },
+      },
+    },
+  ],
+  [
+    {
+      threadId: postId,
+      id: '2',
+      renderedBody: 'test comment 2',
+      voteCount: 0,
+      author: 'testauthor',
+      users: {
+        'testauthor': {
+          profile: {
+            image: {
+              image_url_small: '',
+            },
+          },
+        },
+      },
+    },
+  ],
+];
+
+function mockAxiosReturnPagedComments() {
+  const paramsTemplate = {
+    thread_id: postId,
+    page: undefined,
+    page_size: undefined,
+    requested_fields: 'profile_image',
+  };
+
+  const numPages = mockCommentsPaged.length;
+  let page = 1;
+  for (const comments of mockCommentsPaged) {
+    axiosMock
+      .onGet(commentsApiUrl, { params: { ...paramsTemplate, page } })
+      .reply(200, {
+        results: comments,
+        pagination: {
+          page,
+          numPages,
+        },
+      });
+    page++;
+  }
+}
 
 function renderComponent() {
   const wrapper = render(
@@ -48,7 +114,8 @@ describe('CommentsView', () => {
       threads: {
         threadsById: {
           [postId]: {
-            author: 'test author',
+            id: postId.toString(),
+            author: 'testauthor',
             title: 'test thread',
             voteCount: 0,
             type: 'discussion',
@@ -56,7 +123,7 @@ describe('CommentsView', () => {
           },
         },
         avatars: {
-          'test author': {
+          'testauthor': {
             profile: {
               image: '',
             },
