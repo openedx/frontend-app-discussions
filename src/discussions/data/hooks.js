36f0d9b06df67688f88c236b3c67473ee696ac76
Kshitij Sobti <kshitij@sobti.in>
2022-03-17 10:31:50 +0530

    feat: use resize postmessage API to send size hints to parent When the post list is resized, send a size hint to the parent window (LMS/Learning MFE) so it can resize the discussions MFE iframe dynamically.

--- src/discussions/data/hooks.js
+++ src/discussions/data/hooks.js
@@ -1,9 +1,10 @@
 /* eslint-disable import/prefer-default-export */
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useLocation, useRouteMatch } from 'react-router';
 
+import { getConfig } from '@edx/frontend-platform';
 import { AppContext } from '@edx/frontend-platform/react';
 import { breakpoints, useWindowSize } from '@edx/paragon';
 
@@ -86,4 +87,63 @@ export function useRedirectToThread(courseId) {
 export function useIsOnDesktop() {
   const windowSize = useWindowSize();
   return windowSize.width >= breakpoints.large.minWidth;
+}
+
+/**
+ * Given an element this attempts to get the height of the entire UI.
+ *
+ * @param element
+ * @returns {number}
+ */
+function getOuterHeight(element) {
+  // This is the height of the entire document body.
+  const bodyHeight = document.body.offsetHeight;
+  // This is the height of the container that will scroll.
+  const elementContainerHeight = element.parentNode.clientHeight;
+  // The difference between the body height and the container height is the size of the header footer etc.
+  // Add to that the element's own height and we get the size the UI should be to fit everything.
+  return bodyHeight - elementContainerHeight + element.scrollHeight;
+}
+
+/**
+ * This hook posts a resize message to the parent window if running in an iframe
+ * @param refContainer reference to the component whose size is to be measured
+ */
+export function useContainerSizeForParent(refContainer) {
+  function postResizeMessage(height) {
+    window.parent.postMessage({
+      type: 'plugin.resize',
+      payload: {
+        height,
+      },
+    }, getConfig().LEARNING_BASE_URL);
+  }
+
+  const location = useLocation();
+  const enabled = window.parent !== window;
+
+  const resizeObserver = useRef(new ResizeObserver(() => {
+    /* istanbul ignore if: ResizeObserver isn't available in the testing env */
+    if (refContainer.current) {
+      postResizeMessage(getOuterHeight(refContainer.current));
+    }
+  }));
+
+  useEffect(() => {
+    const container = refContainer.current;
+    const observer = resizeObserver.current;
+    if (container && observer && enabled) {
+      observer.observe(container);
+      postResizeMessage(getOuterHeight(container));
+    }
+
+    return () => {
+      if (container && observer && enabled) {
+        observer.unobserve(container);
+        // Send a message to reset the size so that navigating to another
+        // page doesn't cause the size to be retained
+        postResizeMessage(null);
+      }
+    };
+  }, [refContainer, resizeObserver, location]);
 }
