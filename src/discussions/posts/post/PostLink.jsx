0d05d339f43a32889bb1c78e26096e62dd3cd353
Abdurrahman Asad <abdurrahmanasad133@gmail.com>
2022-04-01 13:01:29 +0500

    fix: post summary box size is large

--- src/discussions/posts/post/PostLink.jsx
+++ src/discussions/posts/post/PostLink.jsx
@@ -44,7 +44,7 @@ function PostLink({
         </div>
       )}
       <div
-        className={classNames('d-flex flex-row flex-fill mw-100 p-3.5 border-primary-500', { 'bg-light-300': post.read })}
+        className={classNames('d-flex flex-row flex-fill mw-100 p-3 border-primary-500', { 'bg-light-300': post.read })}
         style={post.id === postId ? {
           borderRightWidth: '4px',
           borderRightStyle: 'solid',
