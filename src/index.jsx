702087b6c72ce9bc14433f123db7477cd2838a83
Mehak Nasir <67791278+mehaknasir@users.noreply.github.com>
2022-06-01 00:15:45 +0500

    Revert "Temp PR to check build issue. (#177)"
    This reverts commit 2f88aae6ce82863477e1f253674ec4ef914117be.

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,8 @@ import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { MathJaxContext } from 'better-react-mathjax';
+
 import {
   APP_INIT_ERROR, APP_READY, initialize, subscribe,
 } from '@edx/frontend-platform';
@@ -11,6 +13,7 @@ import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
 
 import { DiscussionsHome } from './discussions';
 import appMessages from './i18n';
+import configMaths from './mathjax-config';
 import store from './store';
 
 import './assets/favicon.ico';
@@ -18,9 +21,11 @@ import './index.scss';
 
 subscribe(APP_READY, () => {
   ReactDOM.render(
-    <AppProvider store={store}>
-      <DiscussionsHome />
-    </AppProvider>,
+    <MathJaxContext version={3} config={configMaths}>
+      <AppProvider store={store}>
+        <DiscussionsHome />
+      </AppProvider>
+    </MathJaxContext>,
     document.getElementById('root'),
   );
 });
