57e18119215f6404e392c5c587e125d96cd1f6f3
Mehak Nasir <mehaknasir94@gmail.com>
2022-06-04 01:44:24 +0500

    fix: syntax support extended for latex

--- src/components/HTMLLoader.jsx
+++ src/components/HTMLLoader.jsx
@@ -3,6 +3,24 @@ import PropTypes from 'prop-types';
 
 import MathJax from 'react-mathjax-preview';
 
+const baseConfig = {
+  showMathMenu: true,
+  tex2jax: {
+    inlineMath: [
+      ['$', '$'],
+      ['\\(', '\\)'],
+      ['[mathjaxinline]', '[/mathjaxinline]'],
+    ],
+    displayMath: [
+      ['[mathjax]', '[/mathjax]'],
+      ['$$', '$$'],
+      ['\\[', '\\]'],
+    ],
+  },
+
+  skipStartupTypeset: true,
+};
+
 function HTMLLoader({ htmlNode, componentId, cssClassName }) {
   const isLatex = htmlNode.match(/(\${1,2})((?:\\.|.)*)\1/)
                   || htmlNode.match(/(\[mathjax](.+?)\[\\mathjax])+/)
@@ -10,7 +28,15 @@ function HTMLLoader({ htmlNode, componentId, cssClassName }) {
                   || htmlNode.match(/(\\\[(.+?)\\\])+/);
 
   return (
-    isLatex ? <MathJax math={htmlNode} id={componentId} className={cssClassName} />
+    isLatex ? (
+      <MathJax
+        math={htmlNode}
+        id={componentId}
+        className={cssClassName}
+        sanitizeOptions={{ USE_PROFILES: { html: true } }}
+        config={baseConfig}
+      />
+    )
       // eslint-disable-next-line react/no-danger
       : <div className={cssClassName} id={componentId} dangerouslySetInnerHTML={{ __html: htmlNode }} />
   );
