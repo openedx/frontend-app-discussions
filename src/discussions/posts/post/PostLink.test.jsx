a2cfbc2b3a7c67344625e6684799f03e49ea88ae
Mehak Nasir <mehaknasir94@gmail.com>
2022-06-09 23:14:24 +0500

    fix: tags added for reported/pinned/read/selected posts

--- src/discussions/posts/post/PostLink.test.jsx
+++ src/discussions/posts/post/PostLink.test.jsx
@@ -15,7 +15,7 @@ function renderComponent(post) {
   return render(
     <IntlProvider locale="en">
       <AppProvider store={store}>
-        <PostLink post={post} key={post.id} />
+        <PostLink post={post} key={post.id} isSelected={() => true} />
       </AppProvider>
     </IntlProvider>,
   );
