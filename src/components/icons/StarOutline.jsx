478799e728d486a04ed87ee936af5f46e6224631
Awais Ansari <79941147+awais-ansari@users.noreply.github.com>
2022-06-13 17:03:22 +0500

    fix: post summary card (#186)
    * fix: post summery card
    * test: fix post link fail test cases

---
+++ src/components/icons/StarOutline.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function StarOutline() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      fill="none"
+      viewBox="0 0 20 20"
+    >
+      <path
+        fill="currentColor"
+        d="M18.737 7.7l-5.991-.517-2.342-5.516-2.342 5.525L2.07 7.7l4.55 3.942L5.254 17.5l5.15-3.108 5.15 3.108-1.359-5.858L18.737 7.7zm-8.333 5.133L7.27 14.725l.834-3.567-2.767-2.4 3.65-.316 1.417-3.359 1.425 3.367 3.65.317-2.767 2.4.834 3.566-3.142-1.9z"
+      />
+    </svg>
+  );
+}
