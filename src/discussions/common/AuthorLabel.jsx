becc28ca6de1d00dd2fcf692b1a1ac7bba71709a
AhtishamShahid <ahtisham300@gmail.com>
2022-07-27 14:21:54 +0500

    fix: changed font color for retired user

--- src/discussions/common/AuthorLabel.jsx
+++ src/discussions/common/AuthorLabel.jsx
@@ -34,18 +34,21 @@ function AuthorLabel({
     authorLabelMessage = intl.formatMessage(messages.authorLabelTA);
   }
 
+  const isRetiredUser = author.startsWith('retired__user');
+
   const className = classNames('d-flex align-items-center', labelColor);
 
   const labelContents = (
     <div className={className}>
       <span
         className={classNames('mr-1 font-size-14 font-style-normal font-family-inter font-weight-500', {
-          'text-primary-500': !authorLabelMessage,
+          'text-primary-500': !authorLabelMessage && !isRetiredUser,
+          'text-gray-700': isRetiredUser,
         })}
         role="heading"
         aria-level="2"
       >
-        {author.startsWith('retired__user') ? '(Deactivated)' : author }
+        {isRetiredUser ? '[Deactivated]' : author }
       </span>
       {icon && (
         <Icon
