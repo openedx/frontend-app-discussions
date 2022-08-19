cf34292864463d7282b6941552e9d34c534707e6
Mehak Nasir <mehaknasir94@gmail.com>
2022-08-19 18:38:41 +0500

    fix: anonymous user is not clickable

--- src/discussions/posts/post/PostHeader.jsx
+++ src/discussions/posts/post/PostHeader.jsx
@@ -109,12 +109,12 @@ function PostHeader({
                   && <Badge variant="success">{intl.formatMessage(messages.answered)}</Badge>}
               </div>
             )
-            : <h4 className="mb-0" style={{ lineHeight: '28px' }} aria-level="1" tabIndex={-1} accessKey="h">{post.title}</h4>}
+            : <h4 className="mb-0" style={{ lineHeight: '28px' }} aria-level="1" tabIndex="-1" accessKey="h">{post.title}</h4>}
           <AuthorLabel
             author={post.author || intl.formatMessage(messages.anonymous)}
             authorLabel={post.authorLabel}
             labelColor={authorLabelColor && `text-${authorLabelColor}`}
-            linkToProfile
+            linkToProfile={post.author}
           />
         </div>
       </div>
