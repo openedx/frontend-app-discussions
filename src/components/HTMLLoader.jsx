0bb95f13238e766f5e1c8a25c1bc6ae5260a9609
AsadAzam <asadazam93@gmail.com>
2023-01-26 20:16:25 +0500

    Revert "feat: added mathjax v3 support in platform (#420)"
    This reverts commit ad640611a12fff9cfa39316d22746d85818fb9a6.

--- src/components/HTMLLoader.jsx
+++ src/components/HTMLLoader.jsx
@@ -1,9 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import DOMPurify from 'dompurify';
 
-import { logError } from '@edx/frontend-platform/logging';
+const baseConfig = {
+  showMathMenu: true,
+  tex2jax: {
+    inlineMath: [
+      ['$', '$'],
+      ['\\\\(', '\\\\)'],
+      ['\\(', '\\)'],
+      ['[mathjaxinline]', '[/mathjaxinline]'],
+      ['\\begin{math}', '\\end{math}'],
+    ],
+    displayMath: [
+      ['[mathjax]', '[/mathjax]'],
+      ['$$', '$$'],
+      ['\\\\[', '\\\\]'],
+      ['\\[', '\\]'],
+      ['\\begin{displaymath}', '\\end{displaymath}'],
+      ['\\begin{equation}', '\\end{equation}'],
+    ],
+    processEscapes: true,
+    processEnvironments: true,
+  },
+  skipStartupTypeset: false,
+};
 
 const defaultSanitizeOptions = {
   USE_PROFILES: { html: true },
@@ -11,28 +33,50 @@ const defaultSanitizeOptions = {
 };
 
 function HTMLLoader({ htmlNode, componentId, cssClassName }) {
+  const [loadingState, setLoadingState] = useState(window.MathJax ? 'loaded' : 'loading');
   const sanitizedMath = DOMPurify.sanitize(htmlNode, { ...defaultSanitizeOptions });
+  const mathjaxScript = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_SVG';
 
   useEffect(() => {
-    let promise = Promise.resolve(); // Used to hold chain of typesetting calls
-    function typeset(code) {
-      promise = promise.then(() => window.MathJax.typesetPromise(code()))
-        .catch((err) => logError(`Typeset failed: ${err.message}`));
-      return promise;
-    }
+    let mathjaxScriptTag = document.querySelector(`script[src="${mathjaxScript}"]`);
+    if (!mathjaxScriptTag) {
+      mathjaxScriptTag = document.createElement('script');
+      mathjaxScriptTag.async = true;
+      mathjaxScriptTag.src = mathjaxScript;
 
-    typeset(() => {
-      const math = document.querySelector('#tex2jax_process');
-      math.innerHTML = sanitizedMath;
-      return [math];
-    });
-  }, [sanitizedMath]);
+      const node = document.head || document.getElementsByTagName('head')[0];
+      node.appendChild(mathjaxScriptTag);
+    }
+    const onloadHandler = () => {
+      setLoadingState('loaded');
+      window.MathJax.Hub.Config({ ...baseConfig });
+    };
+    const onerrorHandler = () => {
+      setLoadingState('failed');
+    };
+
+    return () => {
+      mathjaxScriptTag.removeEventListener('load', onloadHandler);
+      mathjaxScriptTag.removeEventListener('error', onerrorHandler);
+    };
+  }, [setLoadingState, baseConfig]);
+
+  useEffect(() => {
+    if (loadingState !== 'loaded') {
+      return;
+    }
+    window.MathJax.Hub.Queue([
+      'Typeset',
+      window.MathJax.Hub,
+      'mathjax-code',
+    ]);
+  }, [sanitizedMath, loadingState]);
 
   return (
-    <div className={cssClassName} id={componentId}>
-      <div id="tex2jax_process" />
+    <div id="mathjax-code">
+      {/* eslint-disable-next-line react/no-danger */}
+      <div className={cssClassName} id={componentId} dangerouslySetInnerHTML={{ __html: sanitizedMath }} />
     </div>
-
   );
 }
 
