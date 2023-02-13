3233d7044d25685970e6d1457b1ea9edf3101354
sundasnoreen12 <sundasnoreen12@ggmail.com>
2023-02-13 15:02:47 +0500

    test: added redux test cases of in context topics

---
+++ src/discussions/in-context-topics/data/__factories__/inContextTopics.factory.js
@@ -0,0 +1,78 @@
+import { Factory } from 'rosie';
+
+import { getApiBaseUrl } from '../../../../data/constants';
+
+Factory.define('topic')
+  .sequence('id', ['topicPrefix'], (idx, topicPrefix) => `${topicPrefix}-${idx}`)
+  .sequence('enabled-in-context', ['enabledInContext'], (idx, enabledInContext) => enabledInContext)
+  .sequence('name', ['topicNamePrefix'], (idx, topicNamePrefix) => `${topicNamePrefix}-${idx}`)
+  .sequence('usage-key', ['usageKey'], (idx, usageKey) => usageKey)
+  .sequence('courseware', ['courseware'], (idx, courseware) => courseware)
+
+  .attr('thread_counts', ['discussionCount', 'questionCount'], (discCount, questCount) => {
+    Factory.reset('thread-counts');
+    return Factory.build('thread-counts', null, { discussionCount: discCount, questionCount: questCount });
+  });
+
+Factory.define('sub-section')
+  .sequence('block_id', (idx) => `${idx}`)
+  .option('topicPrefix', null, '')
+  .sequence('id', ['topicPrefix'], (idx, topicPrefix) => `${topicPrefix}-topic-${idx}`)
+  .sequence('display-name', ['sectionPrefix'], (idx, sectionPrefix) => `Introduction ${sectionPrefix + idx}`)
+  .option('courseId', null, 'course-v1:edX+DemoX+Demo_Course')
+  .sequence('legacy_web_url', ['id', 'courseId'],
+    (idx, id, courseId) => `${getApiBaseUrl}/courses/${courseId}/jump_to/block-v1:${id}?experience=legacy`)
+  .sequence('lms_web_url', ['id', 'courseId'],
+    (idx, id, courseId) => `${getApiBaseUrl}/courses/${courseId}/jump_to/block-v1:${id}`)
+  .sequence('student_view_url', ['id', 'courseId'],
+    (idx, id) => `${getApiBaseUrl}/xblock/block-v1:${id}`)
+  .attr('type', null, 'sequential')
+  .attr('children', ['id', 'display-name', 'courseId'], (id, name, courseId) => {
+    Factory.reset('topic');
+    return Factory.buildList('topic', 2, null, {
+      topicPrefix: `${id}`,
+      enabledInContext: true,
+      topicNamePrefix: `${name}`,
+      usageKey: `${courseId.replace('course-v1:', 'block-v1:')} +type@vertical+block@vertical_`,
+      discussionCount: 1,
+      questionCount: 1,
+    });
+  });
+
+Factory.define('section')
+  .sequence('block_id', (idx) => `${idx}`)
+  .option('topicPrefix', null, '')
+  .sequence('id', ['topicPrefix'], (idx, topicPrefix) => `${topicPrefix}-topic-${idx}`)
+  .attr('courseware', null, true)
+  .sequence('display-name', (idx) => `Introduction ${idx}`)
+  .option('courseId', null, 'course-v1:edX+DemoX+Demo_Course')
+  .sequence('legacy_web_url', ['id', 'courseId'],
+    (idx, id, courseId) => `${getApiBaseUrl}/courses/${courseId}/jump_to/${courseId.replace('course-v1:', 'block-v1:')}+type@chapter+block@${id}?experience=legacy`)
+  .sequence('lms_web_url', ['id', 'courseId'],
+    (idx, id, courseId) => `${getApiBaseUrl}/courses/${courseId}/jump_to/${courseId.replace('course-v1:', 'block-v1:')}+type@chapter+block@${id}`)
+  .sequence('student_view_url', ['id', 'courseId'],
+    (idx, id, courseId) => `${getApiBaseUrl}/xblock/${courseId.replace('course-v1:', 'block-v1:')}+type@chapter+block@${id}`)
+  .attr('type', null, 'chapter')
+  .attr('children', ['display-name'], (name) => {
+    Factory.reset('sub-section');
+    return Factory.buildList('sub-section', 2, null, { sectionPrefix: `${name}-`, topicPrefix: 'section' });
+  });
+
+Factory.define('thread-counts')
+  .sequence('discussion', ['discussionCount'], (idx, discussionCount) => discussionCount)
+  .sequence('question', ['questionCount'], (idx, questionCount) => questionCount);
+
+Factory.define('archived-topics')
+  .attr('id', null, 'archived')
+  .option('courseId', null, 'course-v1:edX+DemoX+Demo_Course')
+  .attr('children', ['id', 'courseId'], (id, courseId) => {
+    Factory.reset('topic');
+    return Factory.buildList('topic', 2, null, {
+      topicPrefix: `${id}`,
+      enabledInContext: false,
+      topicNamePrefix: `${id}`,
+      usageKey: `${courseId.replace('course-v1:', 'block-v1:')} +type@vertical+block@`,
+      discussionCount: 1,
+      questionCount: 1,
+    });
+  });
---
+++ src/discussions/in-context-topics/data/api.test.js
@@ -0,0 +1,72 @@
+import MockAdapter from 'axios-mock-adapter';
+import { Factory } from 'rosie';
+
+import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
+import { initializeMockApp } from '@edx/frontend-platform/testing';
+
+import { initializeStore } from '../../../store';
+import { executeThunk } from '../../../test-utils';
+import { getCourseTopicsApiUrl, getCourseTopicsV3 } from './api';
+import { fetchCourseTopicsV3 } from './thunks';
+
+import './__factories__';
+
+const courseId = 'course-v1:edX+TestX+Test_Course';
+const courseId2 = 'course-v1:edX+TestX+Test_Course2';
+const courseTopicsApiUrl = getCourseTopicsApiUrl();
+
+let axiosMock = null;
+let store;
+
+describe('In context topic api tests', () => {
+  beforeEach(() => {
+    initializeMockApp({
+      authenticatedUser: {
+        userId: 3,
+        username: 'abc123',
+        administrator: true,
+        roles: [],
+      },
+    });
+    axiosMock = new MockAdapter(getAuthenticatedHttpClient());
+    store = initializeStore();
+  });
+
+  afterEach(() => {
+    axiosMock.reset();
+  });
+
+  test('successfully get topics', async () => {
+    axiosMock.onGet(`${courseTopicsApiUrl}${courseId}`)
+      .reply(200, (Factory.buildList('topic', 1, null, {
+        topicPrefix: 'noncourseware-topic',
+        enabledInContext: true,
+        topicNamePrefix: 'general-topic',
+        usageKey: '',
+        courseware: false,
+        discussionCount: 1,
+        questionCount: 1,
+      }).concat(Factory.buildList('section', 2, null, { topicPrefix: 'courseware' })))
+        .concat(Factory.buildList('archived-topics', 2, null)));
+
+    const response = await getCourseTopicsV3(courseId);
+
+    expect(response).not.toBeUndefined();
+  });
+
+  it('failed to fetch topics', async () => {
+    axiosMock.onGet(`${courseTopicsApiUrl}${courseId2}`)
+      .reply(404);
+    await executeThunk(fetchCourseTopicsV3(courseId2), store.dispatch, store.getState);
+
+    expect(store.getState().inContextTopics.status).toEqual('failed');
+  });
+
+  it('denied to fetch topics', async () => {
+    axiosMock.onGet(`${courseTopicsApiUrl}${courseId}`)
+      .reply(403, {});
+    await executeThunk(fetchCourseTopicsV3(courseId), store.dispatch);
+
+    expect(store.getState().inContextTopics.status).toEqual('denied');
+  });
+});
