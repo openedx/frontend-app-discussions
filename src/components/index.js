137795f254a994f10698060cd3e19b8adcefc832
Muhammad Adeel Tajamul <77053848+muhammadadeeltajamul@users.noreply.github.com>
2023-02-17 08:29:06 +0500

    feat: added stats at subsection level in topics tab (#437)
    Co-authored-by: adeel.tajamul <adeel.tajamul@arbisoft.com>

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as PostActionsBar } from '../discussions/posts/post-actions-bar/PostActionsBar';
 export { default as Search } from './Search';
 export { default as TinyMCEEditor } from './TinyMCEEditor';
+export { default as TopicStats } from './TopicStats';
