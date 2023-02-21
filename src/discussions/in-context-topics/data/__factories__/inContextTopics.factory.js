c662310b08fe1f142131886294f91bc5035a9411
sundasnoreen12 <72802712+sundasnoreen12@users.noreply.github.com>
2023-02-21 12:45:08 +0500

    test: added  test cases for v3 Topics and units list page (#440)
    * test: added  test cases for v3 Topics and units list page
    
    * refactor: v3 topics unit test cases
    
    * refactor: v3 topics unit test cases
    
    * refactor: v3 topics unit test cases
    
    * refactor: removed my added commented line and also optimized the code to get stats for section
    
    ---------
    
    Co-authored-by: sundasnoreen12 <sundasnoreen12@ggmail.com>

--- src/discussions/in-context-topics/data/__factories__/inContextTopics.factory.js
+++ src/discussions/in-context-topics/data/__factories__/inContextTopics.factory.js
@@ -8,7 +8,7 @@ Factory.define('topic')
   .sequence('name', ['topicNamePrefix'], (idx, topicNamePrefix) => `${topicNamePrefix}-${idx}`)
   .sequence('usage-key', ['usageKey'], (idx, usageKey) => usageKey)
   .sequence('courseware', ['courseware'], (idx, courseware) => courseware)
-
+  .attr('activeFlags', null, true)
   .attr('thread_counts', ['discussionCount', 'questionCount'], (discCount, questCount) => {
     Factory.reset('thread-counts');
     return Factory.build('thread-counts', null, { discussionCount: discCount, questionCount: questCount });
@@ -27,6 +27,11 @@ Factory.define('sub-section')
   .sequence('student_view_url', ['id', 'courseId'],
     (idx, id) => `${getApiBaseUrl}/xblock/block-v1:${id}`)
   .attr('type', null, 'sequential')
+  .attr('activeFlags', null, true)
+  .attr('thread_counts', ['discussionCount', 'questionCount'], (discCount, questCount) => {
+    Factory.reset('thread-counts');
+    return Factory.build('thread-counts', null, { discussionCount: discCount, questionCount: questCount });
+  })
   .attr('children', ['id', 'display-name', 'courseId'], (id, name, courseId) => {
     Factory.reset('topic');
     return Factory.buildList('topic', 2, null, {
@@ -42,7 +47,7 @@ Factory.define('sub-section')
 Factory.define('section')
   .sequence('block_id', (idx) => `${idx}`)
   .option('topicPrefix', null, '')
-  .sequence('id', ['topicPrefix'], (idx, topicPrefix) => `${topicPrefix}-topic-${idx}`)
+  .sequence('id', ['topicPrefix'], (idx, topicPrefix) => `${topicPrefix}-topic-${idx}-v3`)
   .attr('courseware', null, true)
   .sequence('display-name', (idx) => `Introduction ${idx}`)
   .option('courseId', null, 'course-v1:edX+DemoX+Demo_Course')
@@ -53,9 +58,15 @@ Factory.define('section')
   .sequence('student_view_url', ['id', 'courseId'],
     (idx, id, courseId) => `${getApiBaseUrl}/xblock/${courseId.replace('course-v1:', 'block-v1:')}+type@chapter+block@${id}`)
   .attr('type', null, 'chapter')
-  .attr('children', ['display-name'], (name) => {
+  .attr('children', ['id', 'display-name'], (id, name) => {
     Factory.reset('sub-section');
-    return Factory.buildList('sub-section', 2, null, { sectionPrefix: `${name}-`, topicPrefix: 'section' });
+    return Factory.buildList('sub-section', 2, null, {
+      sectionPrefix: `${name}-`,
+      topicPrefix: 'section',
+      id,
+      discussionCount: 1,
+      questionCount: 1,
+    });
   });
 
 Factory.define('thread-counts')
