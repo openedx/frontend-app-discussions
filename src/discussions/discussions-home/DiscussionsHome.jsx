60f6871227abb94dc37df80146bd8b2336619188
Mehak Nasir <67791278+mehaknasir@users.noreply.github.com>
2023-02-23 14:36:45 +0500

    Revert "fix: conditionally skipped some API calls and deffered script loading to improve performance"
    This reverts commit 0f2ad8b7b4602b6c96efb96dc10a5fc47e9d54fe.

--- src/discussions/discussions-home/DiscussionsHome.jsx
+++ src/discussions/discussions-home/DiscussionsHome.jsx
@@ -50,7 +50,7 @@ export default function DiscussionsHome() {
     courseId, postId, topicId, category, learnerUsername,
   } = params;
 
-  useCourseDiscussionData(courseId, enableInContextSidebar);
+  useCourseDiscussionData(courseId);
   useRedirectToThread(courseId, enableInContextSidebar);
 
   /*  Display the content area if we are currently viewing/editing a post or creating one.
@@ -124,7 +124,7 @@ export default function DiscussionsHome() {
             </Switch>
           )}
         </div>
-        {!enableInContextSidebar && <DiscussionsProductTour />}
+        <DiscussionsProductTour />
       </main>
       {!enableInContextSidebar && <Footer />}
     </DiscussionContext.Provider>
