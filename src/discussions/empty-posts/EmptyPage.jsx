0efe463a6392c417f70804c301ef84f221e8e1ae
ayesha waris <73840786+ayeshoali@users.noreply.github.com>
2023-03-29 21:27:48 +0500

    fix: changes empty state image and text to monochrome

--- src/discussions/empty-posts/EmptyPage.jsx
+++ src/discussions/empty-posts/EmptyPage.jsx
@@ -15,7 +15,7 @@ function EmptyPage({
   fullWidth = false,
 }) {
   const containerClasses = classNames(
-    'min-content-height justify-content-center align-items-center d-flex w-100 flex-column pt-5',
+    'min-content-height justify-content-center align-items-center d-flex w-100 flex-column',
     { 'bg-light-400': !fullWidth },
   );
 
@@ -23,7 +23,7 @@ function EmptyPage({
     <div className={containerClasses}>
       <div className="d-flex flex-column align-items-center">
         <EmptyIcon />
-        <h3 className="pt-3">{title}</h3>
+        <h3 className="pt-3 text-gray-500 font-weight-500">{title}</h3>
         {subTitle && <p className="pb-2">{subTitle}</p>}
         {action && actionText && (
           <Button onClick={action} variant="outline-dark">
