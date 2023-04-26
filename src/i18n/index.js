7ce4566df35b3b0edfb55805152c4d874ea8da78
Ivo Branco <ivo.branco@fccn.pt>
2023-04-26 14:48:54 +0100

    chore(i18n): add languages
    Add new languages: pt-PT, uk, ru,hi, cs, es-AR, es-ES, fa-IR

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -4,14 +4,22 @@ import { messages as paragonMessages } from '@edx/paragon';
 
 import arMessages from './messages/ar.json';
 // no need to import en messages-- they are in the defaultMessage field
+import csMessages from './messages/cs.json';
 import deMessages from './messages/de_DE.json';
 import es419Messages from './messages/es_419.json';
+import esARMessages from './messages/es_AR.json';
+import esESMessages from './messages/es_ES.json';
+import faIRMessages from './messages/fa_IR.json';
 import frMessages from './messages/fr.json';
 import frCAMessages from './messages/fr_CA.json';
 import frFRMessages from './messages/fr_FR.json';
+import hiMessages from './messages/hi.json';
 import itITMessages from './messages/it_IT.json';
 import plMessages from './messages/pl.json';
+import ptPTMessages from './messages/pt_PT.json';
+import ruMessages from './messages/ru.json';
 import trTRMessages from './messages/tr_TR.json';
+import ukMessages from './messages/uk.json';
 import zhcnMessages from './messages/zh_CN.json';
 
 const appMessages = {
@@ -25,6 +33,14 @@ const appMessages = {
   pl: plMessages,
   'tr-tr': trTRMessages,
   'zh-cn': zhcnMessages,
+  'pt-pt': ptPTMessages,
+  uk: ukMessages,
+  ru: ruMessages,
+  hi: hiMessages,
+  cs: csMessages,
+  'es-AR': esARMessages,
+  'es-ES': esESMessages,
+  'fa-IR': faIRMessages,
 };
 
 export default [
