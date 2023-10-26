3f77821a48a7efc215fdfd1ab049dbcff78929dc
sonxauxi2411 <112046655+sonxauxi2411@users.noreply.github.com>
2023-10-26 19:21:22 +0700

    fix: ui

--- src/header/HeaderLearning.jsx
+++ src/header/HeaderLearning.jsx
@@ -13,16 +13,18 @@ const HeaderLearning = ({
 
     return (
     <header className="learning-header">
-        <div className="container-xl py-2 d-flex align-items-center">
-        <a href={`${getConfig().LMS_BASE_URL}/dashboard`} className="logo">
-           <img className="d-block" src={getConfig().LOGO_URL} alt={getConfig().LOGO_URL} />
-        </a>
-        <div className="d-flex align-items-center flex-grow-1 course-title-lockup" style={{ gap:'0.5rem' }}>
-          <span className="d-block font-weight-bold  m-0">{courseOrg} {courseNumber}</span>
-          <span className="d-block m-0  font-weight-bold ">{courseTitle}</span>
+       <div>
+        <div className="container-fluid py-2 d-flex align-items-center">
+          <a href={`${getConfig().LMS_BASE_URL}/dashboard`} className="logo">
+            <img className="d-block" src={getConfig().LOGO_URL} alt={getConfig().LOGO_URL} />
+          </a>
+          <div className="d-flex align-items-center flex-grow-1 course-title-lockup" style={{ gap:'0.5rem' }}>
+            <span className="d-block m-0  font-weight-bold ">{courseOrg} {courseNumber}</span>
+            <span className="d-block m-0  font-weight-bold  ">{courseTitle}</span>
+          </div>
+        {authenticatedUser &&  <AuthenticatedUserDropdown username={authenticatedUser.username} />}
         </div>
-       {authenticatedUser &&  <AuthenticatedUserDropdown username={authenticatedUser.username} />}
-      </div>
+       </div>
     </header>
     )
 }
