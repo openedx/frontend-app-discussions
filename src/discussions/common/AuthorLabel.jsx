6ae5130c147e2b9e03d7944bbf073bc75d8a8f88
vladislavkeblysh <138868841+vladislavkeblysh@users.noreply.github.com>
2023-12-18 13:26:38 +0200

    feat: fixed page styles (#577)

--- src/discussions/common/AuthorLabel.jsx
+++ src/discussions/common/AuthorLabel.jsx
@@ -50,7 +50,7 @@ const AuthorLabel = ({
 
   const authorName = useMemo(() => (
     <span
-      className={classNames('mr-1.5 font-size-14 font-style font-weight-500', {
+      className={classNames('mr-1.5 font-size-14 font-style font-weight-500 author-name', {
         'text-gray-700': isRetiredUser,
         'text-primary-500': !authorLabelMessage && !isRetiredUser,
       })}
