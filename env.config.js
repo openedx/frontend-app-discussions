479a399b25258e0d230a172688968dfe2ccfb480
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2024-07-22 10:09:13 +0300

    refactor: removed dark theme

--- env.config.js
+++ env.config.js
@@ -7,17 +7,11 @@ module.exports = {
     },
     defaults: {
       light: 'light',
-      dark: 'dark',
     },
     variants: {
       light: {
         urls: {
           default: 'https://cdn.jsdelivr.net/npm/@openedx/paragon@alpha/dist/light.min.css',
-        },
-      },
-      dark: {
-        urls: {
-          default: 'https://cdn.jsdelivr.net/npm/@edx/brand-edx.org@alpha/dist/light.min.css',
         },
       },
     },
