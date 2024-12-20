2c7c77c9a5fd21e0c8f8972ef48759cb2a536a46
dattatadepalli <datta.t@talentsprint.com>
2024-12-20 18:27:45 +0530

    Update DiscussionsHome.jsx

--- src/discussions/discussions-home/DiscussionsHome.jsx
+++ src/discussions/discussions-home/DiscussionsHome.jsx
@@ -115,34 +115,37 @@ const DiscussionsHome = ({ intl }) => {
             courseTitle={courseTitle}
           />
         )}
-        {
-          unAuthUser && (
-            // <Hyperlink
-            //   className="text-reset"
-            //   style={{ textDecoration: "underline" }}
-            //   destination={`${getConfig().CONTACT_URL}`}
-            // >
-            // <div>
-            //   {intl.formatMessage({
-            //     id: "learning.enrollment.alert",
-            //     defaultMessage:
-            //       "You must be enrolled in the course to see course content.",
-            //     description:
-            //       "Message shown to indicate that a user needs to enroll in a course prior to viewing the course content.  Shown as part of an alert, along with a link to enroll.",
-            //   })}
-            // </div>
-            <Alert variant="warning" icon={WarningFilled} className="mb-3">
-              {/* <p>You must be enrolled in the course to see course content.</p> */}
-              <FormattedMessage
-                id="learning.enrollment.alert"
-                defaultMessage="You must be enrolled in the course to see course content."
-                description="Message shown to indicate that a user needs to enroll in a course prior to viewing the course content.  Shown as part of an alert, along with a link to enroll."
-              />
-            </Alert>
-          )
+        <div className="mx-5 mt-3">
+          {
+            unAuthUser && (
+              // <Hyperlink
+              //   className="text-reset"
+              //   style={{ textDecoration: "underline" }}
+              //   destination={`${getConfig().CONTACT_URL}`}
+              // >
+              // <div>
+              //   {intl.formatMessage({
+              //     id: "learning.enrollment.alert",
+              //     defaultMessage:
+              //       "You must be enrolled in the course to see course content.",
+              //     description:
+              //       "Message shown to indicate that a user needs to enroll in a course prior to viewing the course content.  Shown as part of an alert, along with a link to enroll.",
+              //   })}
+              // </div>
+              <Alert variant="warning" icon={WarningFilled} className="mb-3">
+                {/* <p>You must be enrolled in the course to see course content.</p> */}
+                <FormattedMessage
+                  id="learning.enrollment.alert"
+                  defaultMessage="You must be enrolled in the course to see course content."
+                  description="Message shown to indicate that a user needs to enroll in a course prior to viewing the course content.  Shown as part of an alert, along with a link to enroll."
+                />
+              </Alert>
+            )
 
-          // </Hyperlink>
-        }
+            // </Hyperlink>
+          }
+        </div>
+
         <main
           className="container-fluid d-flex flex-column p-0 w-100"
           id="main"
