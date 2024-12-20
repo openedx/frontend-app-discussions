72ddcc333cae32b7c29ea7ac1ff97a0722db5850
dattatadepalli <datta.t@talentsprint.com>
2024-12-20 16:15:45 +0530

    Update api.js

--- src/discussions/topics/data/api.js
+++ src/discussions/topics/data/api.js
@@ -2,6 +2,7 @@
 import { getAuthenticatedHttpClient } from "@edx/frontend-platform/auth";
 
 import { getApiBaseUrl } from "../../../data/constants";
+import { camelCaseObject, getConfig } from "@edx/frontend-platform";
 
 export const getCourseTopicsApiUrl = () =>
   `${getApiBaseUrl()}/api/discussion/v1/course_topics/`;
@@ -11,7 +12,16 @@ export async function getCourseTopics(courseId) {
   try {
     const { data } = await getAuthenticatedHttpClient().get(url);
     return data;
-  } catch (e) {
-    console.log(e, "this is error")
+  } catch (error) {
+    const { httpErrorStatus } = error && error.customAttributes;
+    if (httpErrorStatus === 404) {
+      global.location.replace(
+        `${getConfig().LMS_BASE_URL}/discussions/${courseId}/not-found`
+      );
+
+      // return {};
+    }
+    console.log(error, "this is error");
+    throw error;
   }
 }
