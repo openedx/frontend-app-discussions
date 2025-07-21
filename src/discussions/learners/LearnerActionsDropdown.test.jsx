273cfc128216d5716b4f021845059ba94470bb67
eemaanamir <eemaan.amir@gmail.com>
2025-07-21 18:39:37 +0500

    test: added test cases

--- src/discussions/learners/LearnerActionsDropdown.test.jsx
+++ src/discussions/learners/LearnerActionsDropdown.test.jsx
@@ -10,6 +10,7 @@ import { initializeMockApp } from '@edx/frontend-platform';
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 import { AppProvider } from '@edx/frontend-platform/react';
 
+import { ContentActions } from '../../data/constants';
 import { initializeStore } from '../../store';
 import executeThunk from '../../test-utils';
 import { getCourseConfigApiUrl } from '../data/api';
@@ -92,5 +93,61 @@ describe('LearnerActionsDropdown', () => {
       expect(deleteCourseItem).toBeInTheDocument();
       expect(deleteOrgItem).toBeInTheDocument();
     });
+  });
+
+  it('triggers deleteCoursePosts handler when delete-course-posts is clicked', async () => {
+    const mockDeleteCourseHandler = jest.fn();
+    const mockDeleteOrgHandler = jest.fn();
+    renderComponent({
+      userHasBulkDeletePrivileges: true,
+      actionHandlers: {
+        [ContentActions.DELETE_COURSE_POSTS]: mockDeleteCourseHandler,
+        [ContentActions.DELETE_ORG_POSTS]: mockDeleteOrgHandler,
+      },
+    });
+
+    const openButton = await findOpenActionsDropdownButton();
+    await act(async () => {
+      fireEvent.click(openButton);
+    });
+
+    await waitFor(() => expect(screen.queryByTestId('learner-actions-dropdown-modal-popup')).toBeInTheDocument());
+
+    const deleteCourseItem = await screen.findByTestId('delete-course-posts');
+    await act(async () => {
+      fireEvent.click(deleteCourseItem);
+    });
+
+    await waitFor(() => expect(screen.queryByTestId('learner-actions-dropdown-modal-popup')).not.toBeInTheDocument());
+    expect(mockDeleteCourseHandler).toHaveBeenCalled();
+    expect(mockDeleteOrgHandler).not.toHaveBeenCalled();
+  });
+
+  it('triggers deleteOrgPosts handler when delete-org-posts is clicked', async () => {
+    const mockDeleteCourseHandler = jest.fn();
+    const mockDeleteOrgHandler = jest.fn();
+    renderComponent({
+      userHasBulkDeletePrivileges: true,
+      actionHandlers: {
+        [ContentActions.DELETE_COURSE_POSTS]: mockDeleteCourseHandler,
+        [ContentActions.DELETE_ORG_POSTS]: mockDeleteOrgHandler,
+      },
+    });
+
+    const openButton = await findOpenActionsDropdownButton();
+    await act(async () => {
+      fireEvent.click(openButton);
+    });
+
+    await waitFor(() => expect(screen.queryByTestId('learner-actions-dropdown-modal-popup')).toBeInTheDocument());
+
+    const deleteOrgItem = await screen.findByTestId('delete-org-posts');
+    await act(async () => {
+      fireEvent.click(deleteOrgItem);
+    });
+
+    await waitFor(() => expect(screen.queryByTestId('learner-actions-dropdown-modal-popup')).not.toBeInTheDocument());
+    expect(mockDeleteOrgHandler).toHaveBeenCalled();
+    expect(mockDeleteCourseHandler).not.toHaveBeenCalled();
   });
 });
