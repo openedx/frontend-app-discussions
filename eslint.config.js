c2302700d4e4a92cdb0728246dc52ac6d12db339
javier ontiveros <javier.ontiveros@wgu.edu>
2025-08-14 13:32:02 -0600

    chore: eslint configs

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// @ts-check
+
+const { createLintConfig } = require('@openedx/frontend-base/config');
+
+module.exports = createLintConfig(
+  {
+    files: [
+      'src/**/*',
+      'site.config.*',
+    ],
+  },
+  {
+    ignores: [
+      'documentation/*',
+      'jest.config.js',
+      'src/i18n/messages/'
+    ],
+  },
+);
